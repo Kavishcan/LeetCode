fe6fad4683af2fc0e39b56364ce1d5728f1950a4
Kavishcan <kavishcan2002@gmail.com>
2024-05-20 23:12:50 +0530

    question 2

---
+++ 771. Jewels and Stones/main.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    # O(m * n)
+    def numJewelsInStones1(self, jewels, stones):
+
+        count = 0
+        for stone in stones:
+            print(stone)
+            if stone in jewels:
+                count += 1
+        return count
+
+    # O(m + n)
+    def numJewelsInStones2(self, jewels, stones):
+        s = set(jewels)
+        count = 0
+
+        for stone in stones:
+            if stone in s:
+                count += 1
+
+        return count
