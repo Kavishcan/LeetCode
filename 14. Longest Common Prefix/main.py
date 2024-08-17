aec3df85d0d220439db625ea6fe2d871ceac169d
Kavishcan <kavishcan2002@gmail.com>
2024-08-18 01:37:39 +0530

    14. Longest Common Prefix

---
+++ 14. Longest Common Prefix/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    # Time: O(n)
+    def longestCommonPrefix(v: list[str]) -> str:
+        ans=""
+        v=sorted(v)
+        first=v[0]
+        last=v[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans 
+    
+
+print(Solution.longestCommonPrefix(["flower","flow","flight"]))
