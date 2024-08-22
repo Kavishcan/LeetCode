2d38f73efb795199f4114659c66598e7184cbffb
Kavishcan <kavishcan2002@gmail.com>
2024-08-23 01:09:15 +0530

    max number of balloons made the max_balloons value  to infinity

--- 1189. Maximum Number of Balloons/main.py
+++ 1189. Maximum Number of Balloons/main.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxNumberOfBalloons(text: str) -> int:
-        count = 0
         h = {}
-        b = {"b": 1, "a": 1, "l": 2, "o": 2, "n":1}
         for i in text:
             h[i] = h.get(i, 0) + 1
         
-        max_balloons = 0
+        b = {"b": 1, "a": 1, "l": 2, "o": 2, "n":1}
+        max_balloons = float("inf")
         for char in b:
             if char in h:
                 max_balloons = min(max_balloons, h[char] // b[char])
