05b8553881e8d8489813d66c85684bbe8809c1db
Kavishcan <kavishcan2002@gmail.com>
2024-08-25 22:24:31 +0530

    344. Reverse String

---
+++ 344. Reverse String/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(s: list[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        
+        print(s)
+
+    
+    reverseString(["h","e","l","l","o"]) 
